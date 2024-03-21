e5aeb0fc441403ff9fb16d61c29ab43f3f45c9dd
Wojciech Klause <wojtasxj@gmail.com>
2024-03-21 14:12:50 +0100

    Dokonczenie akordeonu

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,7 @@ const select = {
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     };
     renderInMenu(){
@@ -71,6 +72,30 @@ const select = {
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+        clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = thisProduct.element.querySelectorAll(select.all.menuProductsActive);  
+        
+        console.log('activeProduct:', activeProduct);
+        
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+          activeProduct.forEach(() => {
+            thisProduct.element.classList.remove(classNames.menuProduct.wrapperActive);
+          })
+        /* toggle active class on thisProduct.element */
+          thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        
+      });
     }
   };
 
