205beeac16270ec418fc150a8993a84227451128
Wojciech Klause <wojtasxj@gmail.com>
2024-03-21 11:30:49 +0100

    first class done

--- dist/js/script.js
+++ dist/js/script.js
@@ -53,19 +53,44 @@ const select = {
   };
 
   class Product{
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
-
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log('new Product:', thisProduct);
     };
+    renderInMenu(){
+      const thisProduct = this;
+
+      /* generate HTML based on template*/
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
   };
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product ();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+      
     },    
     
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -74,6 +99,7 @@ const select = {
       console.log('settings:', settings);
       console.log('templates:', templates);
     
+    thisApp.initData();
     thisApp.initMenu();
     },
   };
