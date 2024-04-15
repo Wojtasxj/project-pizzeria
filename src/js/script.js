ce3d3808bf31b8bc3a9a392de55134554afbd089
Wojciech Klause <wojtasxj@gmail.com>
2024-04-15 20:14:40 +0200

    convert to json&ajax

--- src/js/script.js
+++ src/js/script.js
@@ -70,6 +70,11 @@ const settings = {
   cart: {
     defaultDeliveryFee: 20,
   },
+  db: {
+    url: '//localhost:3131',
+    products: 'products',
+    orders: 'orders',
+  },
 };
 
 const templates = {
@@ -411,19 +416,29 @@ const app = {
   initMenu: function() {
     const thisApp = this;
     for(let productData in thisApp.data.products) {
-      new Product(productData, thisApp.data.products[productData]);
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }     
   },    
     
   initData: function() {
     const thisApp = this;
-    thisApp.data = dataSource;
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.products;
+    fetch(url)
+      .then(function(rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
+      });  
+    console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
   init: function() {
     const thisApp = this;
     thisApp.initData();
-    thisApp.initMenu();
     thisApp.initCart();
   },
 
